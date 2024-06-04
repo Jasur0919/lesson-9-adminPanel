b2d8b45a4bfba4a48ec6174e80a181a53aa2e871
Jasur0919 <jasurjohn1111@gmail.com>
2024-06-04 16:04:03 +0500

    sdfghjytrs

--- src/pages/Saidber/Albums/Albums.jsx
+++ src/pages/Saidber/Albums/Albums.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Albums = () => {
+  const [user,setUser] = useState([])
+  console.log(user);
+
+
+  useEffect(() =>{
+   axios
+   .get('https://jsonplaceholder.typicode.com/albums')
+   .then((res) =>{
+    setUser(res.data);
+   })
+   .catch(arr =>{
+    console.log('>>>>>>>>>>>>>>>', arr);
+   }) 
+  },[])
+
+  let data = user?.map((el, inx) =>(
+    <tr key={el.id}>
+      <td>{inx+1}</td>
+      <td>{el.userId}</td>
+      <td>{el.title}</td>
+      
+    </tr>
+  ))
   return (
     <div>
         <h1>Albums</h1>
+
+        
+        <table className='table table-bordered table-hover table-striped'>
+              <thead className=''>
+                <tr>
+                  <td>T/R</td> 
+                  <td>Name</td>
+                  <td>Family</td>
+                
+                </tr>
+              </thead>
+              <tbody>
+                  {data}
+              </tbody>
+            </table>
     </div>
   )
 }
--- src/pages/Saidber/Comments/Comments.jsx
+++ src/pages/Saidber/Comments/Comments.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Comments = () => {
+
+  const [user,setUser] = useState([])
+  console.log(user);
+
+
+  useEffect(() =>{
+   axios
+   .get('https://jsonplaceholder.typicode.com/comments')
+   .then((res) =>{
+    setUser(res.data);
+   })
+   .catch(arr =>{
+    console.log('>>>>>>>>>>>>>>>', arr);
+   }) 
+  },[])
+
+  let data = user?.map((el, inx) =>(
+    <tr key={el.id}>
+      <td>{inx+1}</td>
+      <td>{el.name}</td>
+      <td>{el.email}</td>
+      <td>{el.body}</td>
+      
+    </tr>
+  ))
   return (
     <div>
         <h1>Comments</h1>
+
+        <table className='table table-bordered table-hover table-striped'>
+              <thead className=''>
+                <tr>
+                  <td>T/R</td> 
+                  <td>Name</td>
+                  <td>Email</td>
+                  <td>Body</td>
+                
+                </tr>
+              </thead>
+              <tbody>
+                  {data}
+              </tbody>
+            </table>
     </div>
   )
 }
 
 export default Comments
+
+
+
+
